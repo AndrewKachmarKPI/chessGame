d57cc81d00b53b8ed13f7793af9be1ddc436852c
Andrea04172004 <kachmar24@ukr.net>
2022-04-06 21:17:08 +0300

    css styles and other fixes

--- src/main/java/com/chess/chessgame/serviceImpl/GameFileServiceImpl.java
+++ src/main/java/com/chess/chessgame/serviceImpl/GameFileServiceImpl.java
@@ -6,19 +6,18 @@ import com.chess.chessgame.enums.FigureColor;
 import com.chess.chessgame.enums.FigureName;
 import com.chess.chessgame.services.GameFileService;
 import javafx.scene.image.Image;
+
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.util.*;
 
 public class GameFileServiceImpl implements GameFileService {
     @Override
     public List<ChessFigure> getFiguresFromInitFile() {
         List<ChessFigure> figures = new ArrayList<>();
-        File file = new File("src/main/resources/game/init.txt");
+        InputStream file = getFileInputStream("game/init.txt");
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(file));
             String line;
             while ((line = bufferedReader.readLine()) != null) { //&& figures.size() <= 10
                 line = line.trim();
@@ -32,12 +31,12 @@ public class GameFileServiceImpl implements GameFileService {
                 }
             }
             bufferedReader.close();
+            file.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return figures;
     }
-
     @Override
     public void removeFigureFromFile(ChessFigure chessFigure) {
         String removeChessLine = chessFigure.getColor().toString().toLowerCase(Locale.ROOT) + " " +
@@ -70,8 +69,8 @@ public class GameFileServiceImpl implements GameFileService {
     public List<ChessFigure> getAllFigures() {
         List<ChessFigure> figures = new ArrayList<>();
         try {
-            File file = new File("src/main/resources/game/allFigures.txt");
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            InputStream file = getFileInputStream("game/allFigures.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(file));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 line = line.trim();
@@ -79,6 +78,7 @@ public class GameFileServiceImpl implements GameFileService {
                 String name = line.split(" ")[1];
                 figures.add(new ChessFigure(FigureName.valueOf(name.toUpperCase(Locale.ROOT)), FigureColor.valueOf(color.toUpperCase(Locale.ROOT))));
             }
+            file.close();
             bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -118,7 +118,14 @@ public class GameFileServiceImpl implements GameFileService {
 
     @Override
     public Image loadImageByPath(String path) {
-      return new Image(getFileInputStream(path));
+        InputStream inputStream = getFileInputStream(path);
+        Image image = new Image(getFileInputStream(path));
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     private InputStream getFileInputStream(String fileName) {
